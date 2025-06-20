39830415948b5e29863d8e2a5f4a4ad5b31b4aa7
Thorben Bender <bender.thorben0@gmail.com>
2025-06-20 16:21:23 +0200

    New button
    This is just for testing

---
+++ src/app/button/button.component.ts
@@ -0,0 +1,10 @@
+import { Component, Injectable } from '@angular/core';
+
+Component({
+  standalone: false,
+  styles: './button.component.scss',
+  template: './button.component.html',
+});
+export class AppButton {
+  constructor() {}
+}
